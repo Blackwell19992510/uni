bfc50eb5d3b1e7f60c5739d9c81ffde1610ccd8d
Blackwell19992510 <mucciidayday@gmail.com>
2023-10-24 22:46:26 +0300

    .

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Login Page"),
+        title: const Text("LOGIN PAGE"),
         centerTitle: true,
       ),
       body: Column(
